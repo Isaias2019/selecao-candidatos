72a390388d14fd0e7e7a9d1d78337490a451a6ea
isaias2019 <isaiassilva356@gmail.com>
2025-03-24 21:56:35 -0300

    Primeiro commit do projeto selecao-candidato

---
+++ src/main/java/com/selecao_candidato/selecao_candidato/SelecaoCandidatoApplication.java
@@ -0,0 +1,13 @@
+package com.selecao_candidato.selecao_candidato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelecaoCandidatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelecaoCandidatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selecao_candidato/selecao_candidato/SelecaoCandidatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selecao_candidato.selecao_candidato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelecaoCandidatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
